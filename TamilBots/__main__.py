f5e076fab42bc48f11499cef6c60887349d71a2d
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-06 02:52:58 +0300

    Update __main__.py

--- TamilBots/__main__.py
+++ TamilBots/__main__.py
@@ -25,7 +25,7 @@ async def start(client, message):
     chat_id = message.chat.id
     user_id = message.from_user["id"]
     name = message.from_user["first_name"]
-    app.send_video("https://telegra.ph/file/a05f929282c3158544d5d.mp4", caption = 'Selam')
+    app.send_video("BAACAgQAAxkBAAIDHWC8De5we67inU5e4iuKpU2ISuJhAAItCwACY-ThUVsmOCYr1KFaHwQ", caption = start_text)
     if message.chat.type == "private":
         btn = InlineKeyboardMarkup([
             [InlineKeyboardButton("📣 Channel", url="http://t.me/KaybedenlerOrkestrasi"),
