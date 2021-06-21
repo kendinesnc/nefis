0f11991a9faf098ea4f9bd80933af9d6c4b18dba
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-21 15:25:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-API_ID = int(os.getenv("4748268"))
+API_ID = int(os.getenv(4748268))
 API_HASH = os.getenv("3bb985dd19588c90aaad6179dfaa9803")
 BOT_TOKEN = os.getenv("1862900591:AAHdOgND0ojzVl2tWBsJu6GKzDcA78oOvCU")
 DATABASE_URL = os.getenv("DATABASE_URL")
