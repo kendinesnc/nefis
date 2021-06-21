963f06904f412d4acc8cbb0b7f04f91479c886c1
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-21 17:02:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-API_ID = int(os.getenv("5962135"))
-API_HASH = os.getenv("61c06ec69048612b49788f41bbdeb61f")
-BOT_TOKEN = os.getenv("1862900591:AAHdOgND0ojzVl2tWBsJu6GKzDcA78oOvCU")
+API_ID = "5962135
+API_HASH = "61c06ec69048612b49788f41bbdeb61f"
+BOT_TOKEN = "1862900591:AAHdOgND0ojzVl2tWBsJu6GKzDcA78oOvCU"
 DATABASE_URL = os.getenv("DATABASE_URL")
 OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
